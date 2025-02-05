b6c2ae941f2a0f4c6c59b2119d30f53f86279ff7
Harshit Rohilla <harshitrohilla105@gmail.com>
2025-02-05 15:51:28 +0530

    added fevicon and project name

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,9 +9,9 @@ function Contact(){
         <div className="w-2/3 mx-auto text-[#cbe957] flex flex-col items-center flex-grow">
             <h1 className="text-4xl font-semibold mt-20 mb-10">Contact Us</h1>
             <form className="flex flex-col w-full sm:w-2/5 gap-6">
-                <input required name="username" className="px-2 py-2 outline-none w-full border border-[#cbe957] rounded-md" placeholder="Enter Your Name" type="text" name="" id="" />
-                <input required name="email" className="px-2 py-2 outline-none w-full border border-[#cbe957] rounded-md" placeholder="Enter Your Email" type="email" name="" id="" />
-                <textarea required name="message" className="px-2 py-2 h-30 outline-none w-full border border-[#cbe957] rounded-md" placeholder="Enter Your Message" name="" id=""></textarea>
+                <input required name="username" className="px-2 py-2 outline-none w-full border border-[#cbe957] rounded-md" placeholder="Enter Your Name" type="text"/>
+                <input required name="email" className="px-2 py-2 outline-none w-full border border-[#cbe957] rounded-md" placeholder="Enter Your Email" type="email"/>
+                <textarea required name="message" className="px-2 py-2 h-30 outline-none w-full border border-[#cbe957] rounded-md" placeholder="Enter Your Message"></textarea>
                 <button className="w-full border cursor-pointer border-[#cbe957] py-2 rounded-md">SEND</button>
             </form>
         </div>
